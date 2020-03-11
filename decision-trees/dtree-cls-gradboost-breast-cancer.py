3c023e2e407530394540c3e9441eb6dca526b42a
major-phyo-san <phyojupiter9@gamil.com>
2020-03-11 16:46:11 +0630

    dtree-cls-gradboost created

---
+++ decision-trees/dtree-cls-gradboost-breast-cancer.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from matplotlib import pyplot as plt
+
+#load dataset
+dataframe = pd.read_csv("../test-datasets/classification/breast_cancer.csv",sep=",")
+
+#dataframe shuffling
+dataframe = dataframe.reindex(np.random.permutation(dataframe.index))
+
+#training set, test set splitting
+train_set = dataframe.sample(frac=0.8, random_state=200)
+test_set = dataframe.drop(train_set.index)
+
+#training set features and target
+X_train = train_set[["mean radius","mean texture","mean perimeter","mean area","mean smoothness",
+"mean compactness","mean concavity","mean concave points","mean symmetry","mean fractal dimension",
+"radius error","texture error","perimeter error","area error","smoothness error","compactness error",
+"concavity error","concave points error","symmetry error","fractal dimension error","worst radius",
+"worst texture","worst perimeter","worst area","worst smoothness","worst compactness","worst concavity",
+"worst concave points","worst symmetry","worst fractal dimension"]]
+y_train = train_set[["cancerous"]]
+
+#test set features and target
+X_test = test_set[["mean radius","mean texture","mean perimeter","mean area","mean smoothness",
+"mean compactness","mean concavity","mean concave points","mean symmetry","mean fractal dimension",
+"radius error","texture error","perimeter error","area error","smoothness error","compactness error",
+"concavity error","concave points error","symmetry error","fractal dimension error","worst radius",
+"worst texture","worst perimeter","worst area","worst smoothness","worst compactness","worst concavity",
+"worst concave points","worst symmetry","worst fractal dimension"]]
+y_test = test_set[["cancerous"]]
+
+#train the model with Random Forest
+d_tree_gbrt = GradientBoostingClassifier(random_state=0, n_estimators=120, max_depth=1, learning_rate=0.1)
+d_tree_gbrt.fit(X_train,y_train)
+training_accuracy = d_tree_gbrt.score(X_train, y_train)
+test_accuracy = d_tree_gbrt.score(X_test, y_test)
+
+print("Accuracy on training set: {}".format(training_accuracy))
+print("Accuracy on test set: {}".format(test_accuracy))

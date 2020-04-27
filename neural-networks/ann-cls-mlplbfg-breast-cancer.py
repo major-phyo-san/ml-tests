ddec51ba5b24fbc707faaf2363ad1a262327a937
major-phyo-san <phyojupiter9@gamil.com>
2020-04-27 11:55:57 +0630

    Update ann-cls-mlplbfg-breast-cancer.py

--- neural-networks/ann-cls-mlplbfg-breast-cancer.py
+++ neural-networks/ann-cls-mlplbfg-breast-cancer.py
@@ -30,7 +30,7 @@ X_test = test_set[["mean radius","mean texture","mean perimeter","mean area","me
 "worst concave points","worst symmetry","worst fractal dimension"]]
 y_test = test_set[["cancerous"]]
 
-mlp = MLPClassifier(solver='lbfgs',random_state=0)
+mlp = MLPClassifier(solver='lbfgs',random_state=10,hidden_layer_sizes=[6,3], activation="relu",alpha=5)
 mlp.fit(X_train,y_train)
 training_accuracy = mlp.score(X_train,y_train)
 test_accuracy = mlp.score(X_test,y_test)
